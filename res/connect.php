db67742be79f867de09ea23e8734a2309c7a4a02
Md. Samin Irtiza <131145579+samin-irtiza-bjit@users.noreply.github.com>
2023-05-25 15:30:05 +0600

    Update connect.php

--- res/connect.php
+++ res/connect.php
@@ -3,7 +3,7 @@ $username = "tetris";
 $password = "tetrispass";
 $dbname = "tetris";
 // Create connection
-$conn = mysqli_connect("localhost", $username, $password, $dbname);
+$conn = mysqli_connect("192.168.56.110", $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
